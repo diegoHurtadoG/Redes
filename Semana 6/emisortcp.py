6af6aeeff3b2a204efc68e2036e7d07066bcc337
diegoHurtadoG <diegohurtado@ug.uchile.cl>
2020-10-28 22:26:59 -0300

    Actualizado al 28 oct

--- Semana 6/emisortcp.py
+++ Semana 6/emisortcp.py
@@ -5,6 +5,13 @@
 #   -> Las de transmitir, recibir datos y close y accept y eso
 
 # Headers TCP -> Algo que tienen que intercambiarse para cachar que las cosas lleguen bien, ACK
+# Header es informacion basica necesaria: Tipo (ACK o SYN)
+#                                         Nro Secuencia (Para saber que responder)
+
+#El 3 way handshake es:
+# SYN       ->
+# SYN + ACK <-
+# ACK       ->
 
 import socket
 import time
@@ -16,13 +23,19 @@ def stopAndWait(sock, msg, address):
     while True:
         try:
             sock.sendto(msg, address)
-            retmsg, retaddress = sock.recvfrom(64)
+            retmsg, retaddress = sock.recvfrom(64) #Cabmiar esto para que sean mensajes mas grande
             return retmsg, retaddress
         except socket.timeout as e:
             #Aqui deberia hacerle algun cambio al mensaje para que sepa que no es primera vez que mando
             #Sin loss, funciona, con loss se cae (el error esta aqui)
             continue
 
+def addHeader(msg):
+    #Aqui se puede armar algo tipo json, o algo tipo html
+    #Que al mensaje le ponga una estructura consistente
+    #Que contenga si es mensaje SYN o mensaje ACK
+    #Y que contenga un numero para ir comunicandose, este numero es el de seguimiento y no es random
+    pass
 
 cdgram_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 cdgram_socket.settimeout(0.01)
