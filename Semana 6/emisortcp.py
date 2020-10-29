fede55a8c6055578fd81bae4978fcbcf2d30e7fc
diegoHurtadoG <diegohurtado@ug.uchile.cl>
2020-10-29 11:45:26 -0300

    Funciona el handshake y add headers del tcp

--- Semana 6/emisortcp.py
+++ Semana 6/emisortcp.py
@@ -14,7 +14,8 @@
 # ACK       ->
 
 import socket
-import time
+import json
+import random
 
 #Creamos el socket como udp
 import sys
@@ -22,29 +23,69 @@ import sys
 def stopAndWait(sock, msg, address):
     while True:
         try:
+            #addHeader(msg, "Contenido", 0000) #todo
             sock.sendto(msg, address)
-            retmsg, retaddress = sock.recvfrom(64) #Cabmiar esto para que sean mensajes mas grande
+            retmsg, retaddress = sock.recvfrom(64) #Cambiar esto para que sean mensajes mas grande
             return retmsg, retaddress
         except socket.timeout as e:
             #Aqui deberia hacerle algun cambio al mensaje para que sepa que no es primera vez que mando
             #Sin loss, funciona, con loss se cae (el error esta aqui)
             continue
 
-def addHeader(msg):
+
+#Dados un mensaje tipo y numero retorna un json con el formato que estamos usando
+def addHeader(msg,tipo,nro):
     #Aqui se puede armar algo tipo json, o algo tipo html
     #Que al mensaje le ponga una estructura consistente
     #Que contenga si es mensaje SYN o mensaje ACK
-    #Y que contenga un numero para ir comunicandose, este numero es el de seguimiento y no es random
-    pass
+    #Y que contenga un numero para ir comunicandose, este numero es el de seguimiento y es nro de intervalo de bits
+    msg_set = {"Tipo": tipo, "Numero": nro, "Mensaje": msg}
+    msg_json = json.dumps(msg_set)
+    return msg_json
+
+#Funciona bien
+#Dado un socket y un address, conecta al emisor con los parametros.
+def handshake(socket, address):
+    print("Iniciando Handshake ...")
+    numero_ack = random.randint(0, 1000000)
+    msg_json = addHeader("", "ACK", numero_ack)
+
+    print("Enviando primer mensaje de handshake: ", msg_json)
+
+    respuesta, dirret = stopAndWait(socket, msg_json.encode(), address)
+
+    print("Recibida respuesta de handshake: ", respuesta.decode())
+
+    json_respuesta = json.loads(respuesta.decode())
+    if((json_respuesta["Tipo"] == "ACK + SYN") and (json_respuesta["Numero"] == numero_ack + 1)):
+        #Si entre a este if me falta mandar el syn de vuelta
+        msg_syn = addHeader(json_respuesta["Mensaje"] + 1, "SYN", None)
+        print("Enviando mensaje SYN del handshake: ", msg_syn)
+        final, dirRet = stopAndWait(socket, msg_syn.encode(), address)
+    else:
+        final = "No se completo handshake"
+
+    return final
+
+
 
 cdgram_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 cdgram_socket.settimeout(0.01)
+
+#Aqui hago el handshako
+aprobacion_json = handshake(cdgram_socket, ('localhost', 5000))
+aprobacion_json_decoded = aprobacion_json.decode()
+aprobacion = json.loads(aprobacion_json_decoded)
+if(aprobacion["Mensaje"] != "OK"):
+    print("Handshake no se aprobo")
+    raise Exception("No se aprobo la conexion")
 
 #De esta forma de abajo, se deberia llamar por consola de la forma
 # cat prueba | python3 clientudpfile.py
 data = sys.stdin.readlines()
 
 for line in data:
+
     #Creamos el mensaje
     send_message = line.encode()
 
--- Semana 6/receptortcp.py
+++ Semana 6/receptortcp.py
@@ -7,6 +7,18 @@
 # Headers TCP -> Algo que tienen que intercambiarse para cachar que las cosas lleguen bien, ACK
 
 import socket
+import json
+import random
+
+#Dados un mensaje tipo y numero retorna un json con el formato que estamos usando
+def addHeader(msg,tipo,nro):
+    #Aqui se puede armar algo tipo json, o algo tipo html
+    #Que al mensaje le ponga una estructura consistente
+    #Que contenga si es mensaje SYN o mensaje ACK
+    #Y que contenga un numero para ir comunicandose, este numero es el de seguimiento y es nro de intervalo de bits
+    msg_set = {"Tipo": tipo, "Numero": nro, "Mensaje": msg}
+    msg_json = json.dumps(msg_set)
+    return msg_json
 
 print('Creando Socket - Servidor')
 #Creamos el socket UDP
@@ -16,11 +28,34 @@ print('Bindeando socket a puerto 5000')
 #Bindeamos el socket udp al puerto
 dgram_socket.bind(('localhost', 5000))
 
+## Aqui empieza el handshake (El while True de afuera)
 while True:
-    print('Esperando un mensaje en el servidor')
-    msg, address = dgram_socket.recvfrom(1024)
+    print("Partiendo el handshake...")
+    msg_hand_json, address = dgram_socket.recvfrom(1024)
+    print("Mensaje handshake recibido: ", msg_hand_json.decode())
+    msg_hand = json.loads(msg_hand_json.decode())
 
-    print('Mensaje recibido: "' + msg.decode() + '", con direccion: ' + str(address))
-    if (len(msg) > 0):
-        ret = dgram_socket.sendto(msg, address)
-        print('Mensaje enviado de vuelta: ' + msg.decode())
+    if(msg_hand["Tipo"] == "ACK"):
+        num_syn = random.randint(0, 100000)
+        msg_hand2 = addHeader(num_syn, "ACK + SYN", msg_hand["Numero"] + 1)
+        print("Enviando de vuelta el ACK + SYN", msg_hand2)
+        temp = dgram_socket.sendto(msg_hand2.encode(), address)
+        continue
+    elif((msg_hand["Tipo"] == "SYN") and (msg_hand["Mensaje"] == num_syn + 1)):
+        final = addHeader("OK", None, None)
+        print("Enviando mensaje final del handshake", final)
+        temp = dgram_socket.sendto(final.encode(), address)
+    else:
+        print("No se completo el handshake")
+        break
+
+    ## Aqui termino el handshake y si salio bien entra al while de abajo
+
+    while True:
+        print('Esperando un mensaje en el servidor')
+        msg, address = dgram_socket.recvfrom(1024)
+
+        print('Mensaje recibido: "' + msg.decode() + '", con direccion: ' + str(address))
+        if (len(msg) > 0):
+            ret = dgram_socket.sendto(msg, address)
+            print('Mensaje enviado de vuelta: ' + msg.decode())
